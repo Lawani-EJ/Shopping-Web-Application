3364b57d0e940493b68b76be9ea03f697802788e
Lawani-EJ <elyonjohn60@gmail.com>
2024-05-31 12:25:45 +0100

    Shopping cart functionality In this commit the Javascript file fetches and displays products from the fake API store And it will allow users to add products to the cart Showing their items in the cart with the quantity and the price It will allow the users increase, decrease and remove items from their cart Updating the cart's total price and item count.

---
+++ script.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const productList = document.getElementById('product-list');
+    const cartItems = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+    const cartDrawer = document.getElementById('cart-drawer');
+    const overlay = document.getElementById('overlay');
+    const closeCartBtn = document.getElementById('close-cart-btn');
+    const openCartBtn = document.getElementById('open-cart-btn');
+    // const cartCount = document.getElementById('cart-count');
+
+    let cart = [];
+
+    // Getting all products
+    fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(products =>{
+        console.log(products);
+        products.forEach(product =>{
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('product');
+            productDiv.innerHTML = `
+            <img src = "${product.image}" alt = "${product.title}">
+            <span>${product.title}</span>
+            <p>$${product.price}</p>
+            <button onclick="addTocart(${product.id})">Add to Cart</button>
+            `
+            productList.appendChild(productDiv)
+        })
+    })
+
+    window.addTocart = function(id){
+        console.log(id);
+        fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(response => response.json())
+        .then(product => {
+            const cartItem = cart.find(item => item.id === product.id)
+            if(cartItem){
+                cartItem.quantity += 1
+            }else{
+                product.quantity = 1
+                cart.push(product)
+            }
+            //Perform logic here later
+            render()
+        })
+    }
+
+
+    function render(){
+        cartItems.innerHTML = ''
+        let totalItems = 0;
+        let totalPrice = 0;
+        cart.forEach(item => {
+            totalItems += item.quantity;
+            totalPrice += item.price * item.quantity;
+            const cartItem = document.createElement('li')
+            cartItem.innerHTML = `
+            ${item.title} - $ ${item.price} x ${item.quantity}
+            <button onclick="incrementQuantity(${item.id})">+</button>
+            <button onclick="decrementQuantity(${item.id})">-</button>
+            <button onclick="removeFromCart (${item.id})">Remove</button>
+            `;
+            cartItems.appendChild(cartItem)
+        })
+    }
+})
